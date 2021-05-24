836d8d41f6884df15c99657226cd253b07fca334
nur165024 <nur42658@gmail.com>
2021-05-24 17:40:16 +0600

    firebase login page done

---
+++ src/Component/Login/Firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA-fxM3hEoj9MQbm4tgEsJqqsdLwQcrTks",
+    authDomain: "doctor-portal-2021.firebaseapp.com",
+    projectId: "doctor-portal-2021",
+    storageBucket: "doctor-portal-2021.appspot.com",
+    messagingSenderId: "542348914722",
+    appId: "1:542348914722:web:13d072365c2cb2d7251a7b"
+};
+
+export default firebaseConfig;
--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
 import loginImage from '../../images/login-image.png';
 import './Login.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import firebaseConfig from './Firebase';
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
 
 const Login = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        const {email,password} = data;
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log(user);
+        })
+        .catch((error) => {
+            const errorMessage = error.message;
+            console.log(errorMessage);
+        });
+    }
 
     return (
         <section id="userLogin">
@@ -17,12 +38,15 @@ const Login = () => {
                             <div class="form-group">
                                 <label htmlFor="email">User Email</label>
                                 <input type="email" {...register('email', {required:true})} name="email" class="form-control" id="email" placeholder="User Email" />
+                                <p className="text-danger">{errors.email && "Email is required"}</p>
                             </div>
                             <div class="form-group">
                                 <label htmlFor="password">Password</label>
                                 <input type="password" {...register('password', {required:true})} name="password" class="form-control" id="password" placeholder="Password" />
+                                <p className="text-danger">{errors.password && "Password is required"}</p>
                             </div>
-                            <button type="submit" class="btn btn-primary btn-block btn-setColor">Submit</button>
+                            <Link className="text-danger text-right d-block" to="#">Forgot your password</Link>
+                            <button type="submit" class="btn btn-primary btn-block btn-setColor mt-4">Submit</button>
                         </form>
                     </div>
                     <div className="col-md-6">
